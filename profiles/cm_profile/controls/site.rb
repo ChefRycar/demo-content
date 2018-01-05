cfb8f3d11ea0e561ea56f7d3a6eec3c31353bf96
Nick Rycar <rycar@chef.io>
2018-01-05 15:34:43 -0800

    initial commit.

---
+++ profiles/cm_profile/controls/site.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+# copyright: 2017, The Authors
+
+title 'Validate Site'
+
+control 'site-1.0' do
+  title 'Site Status'
+  desc 'Ensure the site returns valid content'
+  describe command('curl -k https://localhost/cart') do
+    its { }
+  end
+
+  describe http('https://localhost/cart', ssl_verify: false) do
+    its('status') { should cmp 200 }
+    its('body') { should cmp 'Planet Express' }
+  end
+end
