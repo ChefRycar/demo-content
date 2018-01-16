53d3d735a4ef94ad668e51fbc86d1d2ec757df70
Nick Rycar <rycar@chef.io>
2018-01-16 15:39:06 -0800

    Adding wrapper controls for separate DB

---
+++ profiles/cm_backend/controls/default.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+# copyright: 2017, The Authors
+
+require_controls 'cm_profile' do
+  control 'env-4.0'
+  control 'ports-2.0'
+end
---
+++ profiles/cm_frontend/controls/default.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+# copyright: 2017, The Authors
+
+include_controls 'cm_profile' do
+  skip_control 'env-4.0'
+  skip_control 'ports-2.0'
+end
