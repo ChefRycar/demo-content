1315a076c8a242c653a4118a5963926bdc739904
Nick Rycar <rycar@chef.io>
2018-01-16 12:27:57 -0800

    Fixing query syntax

--- cookbooks/cm_demo/recipes/frontend.rb
+++ cookbooks/cm_demo/recipes/frontend.rb
@@ -1,6 +1,12 @@
+dbquery = "chef_environment:#{node.chef_environment}" \
+          ' AND recipes:bjc-ecommerce\:\:mysql'
 
-dbhost = search(:node, "chef_environment:#{node.chef_environment} AND recipes:bjc-ecommerce\:\:mysql").first
-node.override['bjc-ecommerce']['db-host'] = dbhost unless node['recipes'].include('mysql')
+dbhost = search(:node, dbquery).first
+if dbhost
+  node.override['bjc-ecommerce']['db-host'] = dbhost['hostname']
+else
+  node.override['bjc-ecommerce']['db-host'] = 'localhost'
+end
 
 include_recipe 'bjc-ecomnmerce::tksetup'
 include_recipe 'bjc-ecommerce::java'
