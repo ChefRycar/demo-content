41b879d339e42ed604f0ce0f0abf38ee23b233f8
Nick Rycar <rycar@chef.io>
2018-01-16 18:10:28 -0800

    updating profile to use remote stuffs

--- profiles/cm_profile/controls/site.rb
+++ profiles/cm_profile/controls/site.rb
@@ -6,7 +6,7 @@ title 'Validate Site'
 control 'site-1.0' do
   title 'Site Status'
   desc 'Ensure the site returns valid content'
-  describe http('https://localhost/cart/', ssl_verify: false) do
+  describe http('https://localhost/cart/', ssl_verify: false, enable_remote_worker: true) do
     its('status') { should cmp 200 }
     its('body') { should include 'Planet Express' }
   end
